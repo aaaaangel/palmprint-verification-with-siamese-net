158f5856e7aa78e3214edfe8a3fc8feae5c1cc42
youweiya <1056957459@qq.com>
2018-06-25 08:58:58 +0800

    前后端链接

--- frontend/js/palmprint.js
+++ frontend/js/palmprint.js
@@ -1,22 +1,78 @@
-var $ = function(sel) {
-    return document.querySelector(sel);
-};
 var $All = function(sel) {
     return document.querySelectorAll(sel);
 };
 
 var name = sessionStorage.getItem('name');
+var left;
+var right;
 var timerId;
+
 function update(){
-    $('#name').innerHTML = name;
+    document.getElementById('name').innerHTML = name;
+    var left_list = document.getElementById('left-list');
+    left_list.innerHTML = "";
+    var right_list = document.getElementById('right-list');
+    right_list.innerHTML = "";
+    ajax();
+    upList(left.data, "left");
+    upList(right.data, "right");
+
 }
+
+function upList(data, hand){
+    dom = document.getElementById(hand+'-list');
+    if(data.length==0){
+        dom.innerHTML = "There is no palmprint picture";
+    }
+    else{
+        var ol = document.createElement('ol');
+        data.forEach(function(item, index){
+            var li  = document.createElement('li');
+            li.innerHTML = 'palmprint ' + item;
+
+            li.addEventListener("touchstart", touchFn);
+            li.addEventListener("touchend", touchFn);
+            var oldTouch;
+
+            function touchFn(ev){
+                switch (ev.type){
+                    case "touchstart" : 
+                        oldTouch = ev.changedTouches[0];
+                        break;
+                    case "touchend" :
+                        var newTouch = ev.changedTouches[0];
+                        // 右滑>100删除
+                        if(newTouch.clientX - oldTouch.clientX > 150){
+                            // console.log("delete"+item);
+                            // if(hand=="left"){
+                            //     if(parseInt(item)==left.max){
+
+                            //     }
+                            // }
+                            if(confirm('确定删除'+item+'?')){
+                                ajaxDelete(item, hand);
+                                update();
+                            }
+                            // data.splice(index, 1);
+                        }
+                        break;
+                }
+            }
+
+            ol.appendChild(li);
+        });
+        
+        dom.appendChild(ol);
+    }
+}
+
 window.onload = function(){
-    var palmprint = $('#return');
+    var palmprint = document.getElementById('return');
     palmprint.addEventListener('click', function(){
         window.location.href="index.html";
     },false);
 
-    var change_name = $('#name');
+    var change_name = document.getElementById('name');
     change_name.addEventListener("touchstart", touchFn);
     change_name.addEventListener("touchmove", touchFn);
     change_name.addEventListener("touchend", touchFn);
@@ -50,3 +106,31 @@ window.onload = function(){
 
     update();
 }
+
+function ajax(){
+    $.ajax({
+        type: "get",
+        async: false,
+        url: 'http://127.0.0.1:8080/',
+        data: {"type":1 , "name": name},
+        success:function(response){
+            // alert(response);
+            var temp = JSON.parse(response);
+            left = temp.left;
+            // alert(left);
+            right = temp.right;
+        }
+    });
+};
+
+function ajaxDelete(del, hand){
+    $.ajax({
+        type: "get",
+        async: false,
+        url: 'http://127.0.0.1:8080/',
+        data : {"type":2,"name":name, "hand": hand, "filename": del},
+        success:function(response){
+
+        }
+    });
+};
